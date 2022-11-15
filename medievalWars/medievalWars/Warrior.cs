5af854a0d57056ee71331512b9eadc6768f92b1e
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2022-11-15 19:38:29 +0100

    Map, Warrior, Archer, Wizard osztály létrehozva. Warrior, Archer, Wizard osztályban a név, életerő, szint, tapasztalati pont, pénz, és kaszt property get metódusai létrehozva.

---
+++ medievalWars/medievalWars/Warrior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medievalWars
+{
+    internal class Warrior
+    {
+        private string name = "Geralt";
+        private int health;
+        private int level;
+        private int experience;
+        private int money;
+        private string @class = "warrior";
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+        }
+
+        public int Level
+        {
+            get { return level; }
+        }
+
+        public int Experience
+        {
+            get { return experience; }
+        }
+
+        public int Money
+        {
+            get { return money; }
+        }
+
+        public string Class
+        {
+            get { return @class; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ medievalWars/medievalWars/Wizard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medievalWars
+{
+    internal class Wizard
+    {
+        private string name = "Fiona";
+        private int health;
+        private int level;
+        private int experience;
+        private int money;
+        private string @class = "wizard";
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+        }
+
+        public int Level
+        {
+            get { return level; }
+        }
+
+        public int Experience
+        {
+            get { return experience; }
+        }
+
+        public int Money
+        {
+            get { return money; }
+        }
+
+        public string Class
+        {
+            get { return @class; }
+        }
+    }
+}
