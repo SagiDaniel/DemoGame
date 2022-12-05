c3aaf0f3cd5d7a435cbebef7a5772c5cd31cc3ce
th3w0rthy <nemethszabolcs.daniel02@gmail.com>
2022-12-05 20:30:48 +0100

    nev kesz, eletero kesz, szint kesz, tapasztalatpont kesz, penz kesz, osztaly kesz, inventory kesz, skill folyamatban, tamadasok/vedekezes folyamatban

--- DemoGame/DemoGame/Warrior.cs
+++ DemoGame/DemoGame/Warrior.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,153 @@ namespace DemoGame
 {
     internal class Warrior
     {
+        //nev
+        private string name = "Geralt";
+        public string GetName
+        {
+            get { return name; }
+        }
+        //eletero
+        private int health = 100;
+        public int GetHealth
+        {
+            get { return health; }
+        }
+        public int SetHealth
+        {
+            set { health = value; }
+        }
+        //szint
+        private int level = 1;
+        public int GetLevel
+        {
+            get { return level; }
+        }
+        public int SetLevel
+        {
+            set { level = value; }
+        }
+        //tapasztalatpont
+        private int experience = 0;
+        public int GetExperience
+        {
+            get { return experience; }
+        }
+        public int SetExperience
+        {
+            set { experience = value; }
+        }
+        //penz
+        private int money = 0;
+        public int GetMoney
+        {
+            get { return money; }
+        }
+        public int SetMoney
+        {
+            set { money = value; }
+        }
+        //osztaly
+        private string @class = "warrior";
+        public string GetClass
+        {
+            get { return @class; }
+        }
+        //inventory
+        private int[] inventory;
+        public int[] GetInventory
+        {
+            get { return inventory; }
+        }
+        public int[] SetInventory
+        {
+            set { inventory = value; }
+        }
+        //skill
+        private Dictionary<string, bool> skill;
+        public Dictionary<string, bool> GetSkill
+        {
+            get { return skill; }
+        }
+        public Dictionary<string, bool> SetSkill
+        {
+            set { skill = value; }
+        }
+        public void Skill()
+        {
+
+        }
+        //location
+
+        //basic attack 1
+        private int basicAttack1;
+        public int GetBasicAttack1
+        {
+            get { return basicAttack1; }
+        }
+        public int SetBasicAttack1
+        {
+            set { basicAttack1 = value; }
+        }
+        public void BasicAttack1()
+        {
+
+        }
+        //basic attack 2
+        private int basicAttack2;
+        public int GetBasicAttack2
+        {
+            get { return basicAttack2; }
+        }
+        public int SetBasicAttack2
+        {
+            set { basicAttack2 = value; }
+        }
+        public void BasicAttack2()
+        {
+
+        }
+        //basic attack 3
+        private int basicAttack3;
+        public int GetBasicAttack3
+        {
+            get { return basicAttack3; }
+        }
+        public int SetBasicAttack3
+        {
+            set { basicAttack3 = value; }
+        }
+        public void BasicAttack3()
+        {
+
+        }
+        //ulti 
+        private int ultimateAttack;
+        public int GetUltimateAttack
+        {
+            get { return ultimateAttack; }
+        }
+        public int SetUltimateAttack
+        {
+            set { ultimateAttack = value; }
+        }
+        public void UltimateAttack()
+        {
+
+        }
+        //defence
+        private int defence;
+        public int GetDefence
+        {
+            get { return defence; }
+        }
+        public int SetDefence
+        {
+            set { defence = value; }
+        }
+        public void Defence()
+        {
+
+        }
     }
 }
